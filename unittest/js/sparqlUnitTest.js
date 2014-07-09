5c34d9548330dba565d71f7f0ef4eaa0556bc2e1
saifulnipo <niposust@gmail.com>
2014-07-09 11:02:53 +0200

    code clean up about test.

--- unittest/js/sparqlUnitTest.js
+++ unittest/js/sparqlUnitTest.js
@@ -2,11 +2,6 @@
  * This file contain the unit test of libs/sparql.js file
  *
  * @authors : A Q M Saiful Islam
- * @dependency:
- * {
- *   sparql.js
- *   highlight.js
- * }
  *
  */
 /*global QUnit:false, sparql :false */
@@ -15,9 +10,5 @@ var sparqlUnitTest = {
 
     testIsSparqlServerUrlValid : function(serverUrl) {
         return (sparql.SERVER_ADDRESS === serverUrl);
-    },
-
-
-
-
+    }
 };
