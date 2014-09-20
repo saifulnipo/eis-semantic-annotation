9591f08f5aca633a29b7db157a9ade455f107c7b
Mirsattar <sattar.seyidov@gmail.com>
2014-09-20 13:37:24 +0200

    data cube  insertion
    Added test cases for data cube insertion check

--- unittest/js/allTest.js
+++ unittest/js/allTest.js
@@ -10,42 +10,14 @@ QUnit.test( "Sparql Server Url Validity", function( assert ) {
     var serverUrl = "http://localhost:8890/sparql";
     assert.ok( true === sparqlUnitTest.testIsSparqlServerUrlValid(serverUrl), "Sparql End point URL is valid!" );
 });
+
 QUnit.test( " Camel Case test ", function( assert ) {
     assert.ok( true === sparqlUnitTest.convertCamelCaseSuccessTest(), " Camel case test success!" );
     assert.ok( false === sparqlUnitTest.convertCamelCaseFailTest(), " Camel case test fail!" );
 
 });
 
-
-
-//QUnit.module( "Sparql Response Parser Unit Test" );
-//QUnit.test( "Response Parser", function( assert ) {
-//    assert.ok( true === sparqlResponseParserUnitTest.testIsParsePropertySuccess(), "Parsing success!" );
-//    assert.ok( false === sparqlResponseParserUnitTest.testIsParsePropertySuccess(), "Parsing failed!" );
-//});
-
-
-
-QUnit.module( "Progressbar Test Unit Test" );            
-QUnit.test( " progressbar test ", function( assert ) {
-    assert.ok( true === progressbarUnitTest.ShowProgressbarUnitTest(), " Progress Bar test success!" );
-});
-
-
-
-QUnit.module( "dataCubeSparql Unit Test" ); 
-QUnit.test( "data cube sparql getDocumentURI test", function( assert ) {
-    assert.ok( true == dataCubeSparqlUnitTest.dataCubeSparqlGetDocumentURITest(), " Get document URI test is success!" );
-});
-
-
-QUnit.test( "data cube sparql getDataStructureDefinition test", function( assert ) {
-    assert.ok( true == dataCubeSparqlUnitTest.dataCubeSparqlgetDataStructureDefinition(), "Data Structure Definition test is success!" );
-});
- 
-QUnit.test( "dataCubeSparql GetDataSet Test", function( assert ) {
-    assert.ok( true == dataCubeSparqlUnitTest.dataCubeSparqlGetDataSet(10), "Data Set test is success!" );
-});
-console.log(dataCubeSparql.getDataSet(10));
+QUnit.module("DataCube testing");
+dataCubeSparqlUnitTest.testDataCube(QUnit);
 
  
--- unittest/js/dataCubeSparqlUnitTest.js
+++ unittest/js/dataCubeSparqlUnitTest.js
@@ -1,24 +1,221 @@
 var dataCubeSparqlUnitTest = {
 
-    dataCubeSparqlGetDocumentURITest: function() {
-        return  ("http://eis.iai.uni-bonn.de/semann/pdf/EIS%20Semantic%20Web%20Annotation%20::%20Unit%20Test#page=undefined?name=table1" == dataCubeSparql.getDocumentURI());
+    /**
+     * Sample data to to insert
+     */
+    sampleData : [
+        ['columnHeader1', 'columnHeader2', 'columnHeader3'],
+        ['Hello',         'World', 'Sample'],
+        ['data',          'is', 'Inserted'],
+        ['Hope',          'it', 'works']
+    ],
+
+    /**
+     * All the
+     * @param QUnit
+     */
+    testDataCube : function (QUnit) {
+
+        var temp_name = scientificAnnotation.GRAPH_NAME_EIS;
+        scientificAnnotation.GRAPH_NAME_EIS = 'TestEisAnnotation';
+
+        dataCubeSparql.addAnnotation(dataCubeSparqlUnitTest.sampleData, true);
+
+        QUnit.asyncTest( "Testing Dimension and property", function( assert ) {
+            expect( 1 );
+            setTimeout(function() {
+                dataCubeSparqlUnitTest.isDimensionPropertyAvailable(assert);
+                QUnit.start();
+            }, 1000);
+        });
+
+        QUnit.asyncTest( "Testing data set insertion", function( assert ) {
+            expect( 1 );
+            setTimeout(function() {
+                dataCubeSparqlUnitTest.isDataSetInserted(assert);
+                QUnit.start();
+            }, 1000);
+        });
+
+        QUnit.asyncTest( "Testing Column header insertion", function( assert ) {
+            expect( 1 );
+            setTimeout(function() {
+                dataCubeSparqlUnitTest.isColumnHeaderInserted(assert);
+                QUnit.start();
+            }, 1000);
+        });
+
+        QUnit.asyncTest( "Testing rows insertion ", function( assert ) {
+            expect( 1 );
+            setTimeout(function() {
+                dataCubeSparqlUnitTest.isObserverInserted(assert);
+                QUnit.start();
+            }, 1000);
+        });
+
+        QUnit.asyncTest( "Cleaning up the test database ", function( assert ) {
+            expect( 1 );
+            setTimeout(function() {
+                dataCubeSparqlUnitTest.clearGraph(scientificAnnotation.GRAPH_NAME_EIS, assert);
+                scientificAnnotation.GRAPH_NAME_EIS = temp_name;
+                QUnit.start();
+            }, 1000);
+        });
     },
 
+    /**
+     * Check if the
+     * @param assert
+     */
+    isColumnHeaderInserted: function (assert) {
+        var query =
+                'prefix semann: <' + dataCubeSparql.PREFIX_SEMANN + '>' + '\n' +
+                'prefix ex: <' + dataCubeSparql.PREFIX_EX + '>' + '\n' +
+                'ASK ' + '\n' +
+                '{' + '\n' +
+                    'ex:table1SliceC1 semann:columnHeader "columnHeader1".' + '\n' +
+                    'ex:table1SliceC2 semann:columnHeader "columnHeader2".' + '\n' +
+                    'ex:table1SliceC3 semann:columnHeader "columnHeader3"' + '\n' +
+                '}';
 
-	dataCubeSparqlgetDataStructureDefinition: function() {
-		return ("ex:dsdTable1 a qb:DataStructureDefinition ;\nqb:component [ qb:dimension ex:table1Row ; qb:order 1 ];\nqb:component [ qb:dimension ex:table1Column ;qb:order 2 ];\nqb:component [ qb:measure semann:value ] ." == dataCubeSparql.getDataStructureDefinition());
-	},
+        $.ajax({
+            type: "POST",
+            url: sparql.SERVER_ADDRESS,
+            data: {
+                query: query,
+                format: "application/json"
+            },
+            success: function (response) {
+                assert.ok (response.boolean, 'Column header successfully inserted');
+            },
+            error: function (jqXHR, exception) {
+                assert.ok (false, 'Column header was not inserted');
+            }
+        });
+    },
 
-	dataCubeSparqlGetDataSet: function() {
-		return ("ex:table1 a qb:DataSet ;\ndct:isPartOf <http://eis.iai.uni-bonn.de/semann/pdf/EIS%20Semantic%20Web%20Annotation%20::%20Unit%20Test#page=undefined?name=table1> ;\nqb:structure ex:dsdTable1 ;\nqb:slice ex:sliceC1,ex:sliceC2,ex:sliceC3,ex:sliceC4,ex:sliceC5,ex:sliceC6,ex:sliceC7,ex:sliceC8,ex:sliceC9,ex:sliceC10 ." == dataCubeSparql.getDataSet(10));
+    /**
+     * Check if the inserted item rows available
+     * Note: here we are checking the 1st rows is available or not
+     * @param assert
+     */
+    isObserverInserted: function (assert) {
+        var query =
+            'prefix semann: <' + dataCubeSparql.PREFIX_SEMANN + '>' + '\n' +
+            'prefix ex: <' + dataCubeSparql.PREFIX_EX + '>' + '\n' +
+            'ASK ' + '\n' +
+            '{' + '\n' +
+                'ex:table1R1C1 semann:value "Hello".' + '\n' +
+                'ex:table1R1C2 semann:value "World".' + '\n' +
+                'ex:table1R1C3 semann:value "Sample"' + '\n' +
+            '}';
 
-	}
+        $.ajax({
+            type: "POST",
+            url: sparql.SERVER_ADDRESS,
+            data: {
+                query: query,
+                format: "application/json"
+            },
+            success: function (response) {
+                assert.ok (response.boolean, 'Observation successfully inserted');
+            },
+            error: function (jqXHR, exception) {
+                assert.ok (false, 'Failed to insert observation');
+            }
+        });
+    },
 
+    /**
+     * Check if the inserted item rows available
+     * Note: here we are checking the 1st rows is available or not
+     * @param assert
+     */
+    isDataSetInserted: function (assert) {
+        var query =
+            'prefix qb: <' + dataCubeSparql.PREFIX_CUBE + '>' + '\n' +
+            'prefix ex: <' + dataCubeSparql.PREFIX_EX + '>' + '\n' +
+            'prefix ex: <' + dataCubeSparql.PREFIX_EX + '>' + '\n' +
+            'ASK ' + '\n' +
+            '{' + '\n' +
+                'ex:table1R1C1 qb:dataSet ex:table1.' + '\n' +
+            '}';
 
+        $.ajax({
+            type: "POST",
+            url: sparql.SERVER_ADDRESS,
+            data: {
+                query: query,
+                format: "application/json"
+            },
+            success: function (response) {
+                assert.ok (response.boolean, 'Data Set Inserted success');
+            },
+            error: function (jqXHR, exception) {
+                assert.ok (false, 'Failed to insert data set');
+            }
+        });
+    },
 
+    /**
+     * Check if the dimensions is available in the backend
+     * @return void
+     */
+    isDimensionPropertyAvailable: function (assert) {
+        var query =
+            'prefix qb: <' + dataCubeSparql.PREFIX_CUBE + '>' + '\n' +
+                'prefix semann: <' + dataCubeSparql.PREFIX_SEMANN + '>' + '\n' +
+                'ASK ' + '\n' +
+                '{' + '\n' +
+                    'semann:column ?o qb:DimensionProperty.' + '\n' +
+                    'semann:row ?o qb:DimensionProperty.' + '\n' +
+                    'semann:value ?o qb:MeasureProperty' + '\n' +
+                '}';
+
+        $.ajax({
+            type: "POST",
+            url: sparql.SERVER_ADDRESS,
+            data: {
+                query: query,
+                format: "application/json"
+            },
+            success: function (response) {
+                assert.ok(response.boolean, 'Dimension and property found success test data');
+            },
+            error: function (jqXHR, exception) {
+                assert.ok (false, 'Failed to insert dimension and property');
+            }
+        });
+    },
+
+    /**
+     * Clean up the test data base
+     *
+     * @param graphName
+     */
+    clearGraph : function (graphName, assert) {
+
+        if (graphName === null || $.trim(graphName) === '') {
+            return;
+        }
+
+        var query = "CLEAR GRAPH <" + graphName +">;";
+
+        $.ajax({
+            type: "POST",
+            url: sparql.SERVER_ADDRESS,
+            data: {
+                query: query,
+                format: "application/json"
+            },
+
+            success: function (response) {
+                assert.ok(true, 'Successfully clean up test data');
+            },
+
+            error: function (jqXHR, exception) {
+                assert.ok(false, 'Failed to clean up data');
+            }
+        });
+    }
 };
-
-
-
- 
-
